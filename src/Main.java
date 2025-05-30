a00397a969d3f7857da17cbdd1cba0068668ab5c
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-05-30 00:06:51 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+// Declare variáveis para armazenar:
+// - Seu nome
+// - Sua idade
+// - Sua altura
+// - Se é estudante (true/false)
+public  class Main {
+
+    public static void main(String... args) {
+        String nome = "luka";
+        int idade = 24;
+        Double altura = 1.75;
+        Boolean estudante = true;
+
+        System.out.println("Nome: " + nome );
+        System.out.println("Idade: " + idade );
+        System.out.println("Altura: " + altura );
+        System.out.println("Estudante: " + estudante );
+
+    }
+
+}
+
